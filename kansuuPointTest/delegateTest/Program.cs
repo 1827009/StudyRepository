d1170f4d2dbefbb3beedc89b5216267c2ac3d481
1827009 <LV0018MAX9999@gmail.com>
2022-04-18 11:53:28 +0900

    関数ポインタ

---
+++ kansuuPointTest/delegateTest/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    delegate int MyDelegate(string text);
+    static void Main(string[] args)
+    {
+        MyDelegate myDelegate=new MyDelegate((new Samples()).Sample);
+        myDelegate+=(new Samples()).Sample2;
+        myDelegate+=(new Samples()).Sample3;
+
+        myDelegate("SampleText");
+    }
+
+}
+class Samples
+{    
+    public int Sample(string text){
+        Console.WriteLine(text);
+        return 1;
+    }
+    public int Sample2(string text){
+        Console.WriteLine(text+"2");
+        return 1;
+    }
+    public int Sample3(string text){
+        Console.WriteLine(text+"3");
+        return 1;
+    }
+}
